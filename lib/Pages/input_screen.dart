a91081d41fa6a4c169cdb73aad03c12428d4be6f
Fareed <fks19850@gmail.com>
2019-12-24 21:58:40 +0500

    Home page Changes

--- lib/Pages/input_screen.dart
+++ lib/Pages/input_screen.dart
@@ -1,15 +1,25 @@
-import 'package:addpost/Pages/Home.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:addpost/Utils.dart';
+
+typedef OnPost(String postText);
 
 class AddPost extends StatefulWidget {
+  final OnPost onPost;
+  AddPost(this.onPost);
+
   @override
   _AddPostState createState() => _AddPostState();
 }
 
 class _AddPostState extends State<AddPost> {
-  var _postTextCtrl = new TextEditingController();
+  TextEditingController _postTextCtrl;
+
+  @override
+  void initState() {
+    super.initState();
+    _postTextCtrl = TextEditingController();
+  }
+
   @override
   Widget build(BuildContext context) {
 
@@ -23,61 +33,73 @@ class _AddPostState extends State<AddPost> {
             children: <Widget>[
               Text("Hello Fareed,", style: TextStyle(
                   color: Colors.black,
-                  fontFamily: 'Nunito',
-                  fontSize: 23,
+                  fontFamily: 'OpenSans',
+                  fontSize: 28,
                   fontStyle: FontStyle.italic)
               )
             ]
           )
       ),
-      body: SingleChildScrollView(
-        child: Container(
-            margin: EdgeInsets.only(top: 20),
-            padding: EdgeInsets.zero,
-            child: Column(
-              children: <Widget>[
-                Row(
-                  crossAxisAlignment: CrossAxisAlignment.end,
-                  children: <Widget>[
-                    Expanded(
-                      child: TextField(
-                          controller: _postTextCtrl,
-                          maxLines: 8,
-                          textAlign: TextAlign.left,
-                          decoration: InputDecoration(hintText: "Hello Pakacademy ...", contentPadding: const EdgeInsets.only(top: 10, left: 20, right: 20)),
-                          style: TextStyle(fontSize: 25, fontStyle: FontStyle.italic)
-                      )
-                    )
-                  ]
-                ),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: <Widget>[
-                    SizedBox(
-                      child: RaisedButton(
-                        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30.0)),
-                        padding: EdgeInsets.symmetric(horizontal: 25, vertical: 18),
-                        color: Color(0xFF165ACE),
-                        onPressed: () {
-                         navigate(context, Home(value: _postTextCtrl.text));
-                         _postTextCtrl.clear();
-                        },
-                        child:
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: <Widget>[
-                              Text('Write Post', style: TextStyle(color: Colors.white, fontStyle: FontStyle.italic, fontSize: 21)),
-                              Icon(Icons.add, color: Colors.white),
-                            ]
-                          )
+      body: Container(
+        child: ListView(
+            children: <Widget>[
+              Row(
+                children: <Widget>[
+                  Expanded(
+                    child: TextField(
+                        controller: _postTextCtrl,
+                        textAlign: TextAlign.left,
+                        maxLines: 8,
+                        decoration: InputDecoration(hintText: "Hello Pakacademy ...",
+                            contentPadding: const EdgeInsets.only(top: 20, left: 20, right: 20)
                         ),
-                        width: MediaQuery.of(context).size.width-100,
+                        style: TextStyle(
+                            color:Colors.black,
+                            fontFamily: 'OpenSans',
+                            fontSize: 25,
+                            fontStyle: FontStyle.italic
+                        )
                     )
-                  ]
+                  )
+                ]
+              ),
+              Container(
+                  height: MediaQuery.of(context).size.height-400,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: <Widget>[
+                      SizedBox(
+                        child:
+                        RaisedButton(
+                            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30.0)),
+                            padding: EdgeInsets.symmetric(horizontal: 23, vertical: 10),
+                            color: Color(0xFF165ACE),
+                            onPressed: () {
+                              widget.onPost(_postTextCtrl.text);
+                              Navigator.of(context).pop();
+                              _postTextCtrl.clear();
+                            },
+                            child: Row(
+                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                children: <Widget>[
+                                  Text('Write Post', style: TextStyle(
+                                    color: Colors.white,
+                                    fontFamily: 'OpenSans',
+                                    fontSize: 22,
+                                    fontStyle: FontStyle.italic
+                                    )
+                                  ),
+                                  Icon(Icons.add, size: 30, color: Colors.white)
+                                ]
+                              )
+                        ),
+                        width: MediaQuery.of(context).size.width-70,
+                      )
+                    ]
                 )
-              ]
-            )
-        ),
+              )
+            ]
+        )
       )
     );
   }
